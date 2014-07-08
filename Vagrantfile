0c97fc8843ee0c1d2b403a9f26e38371ad0a2e87
charles-alan.alexander <charles-alan.alexander@hp.com>
2014-07-08 17:08:01 -0600

    poc/example

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,8 @@
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
 
-Vagrant.require_version ">= 1.5.0"
+# Vagrant.require_version ">= 1.5.0"
+vconfig = YAML::load_file("config.yml")
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # All Vagrant configuration is done here. The most common configuration
@@ -27,7 +28,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # via the IP. Host-only networks can talk to the host machine as well as
   # any other machines on the same network, but cannot be accessed (through this
   # network interface) by any external networks.
-  config.vm.network :private_network, type: "dhcp"
+  #config.vm.network :private_network, type: "dhcp"
+
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
@@ -69,8 +71,12 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # to skip installing and copying to Vagrant's shelf.
   # config.berkshelf.except = []
 
+  config.ssh.forward_agent = true
+  config.ssh.private_key_path = ['~/.vagrant.d/insecure_private_key', vconfig['private_key']]
+
   config.vm.provision :chef_solo do |chef|
     chef.json = {
+      public_key: File.read(vconfig['public_key']),
       mysql: {
         server_root_password: 'rootpass',
         server_debian_password: 'debpass',
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,108 +7,116 @@
 # All rights reserved - Do Not Redistribute
 #
 
-recipes = [
-	"apt",
-	"build-essential",
-	"git",
-	"php",
-	"apache2",
-	"apache2::mod_php5",
-	"apache2::mod_rewrite",
-	"mysql::server",
-	"composer",
-	"nodejs::install_from_source",
-]
+Chef::Log.warn(node["public_key"])
 
-for r in recipes do
-  include_recipe r
+file "/home/vagrant/.ssh/id_rsa.pub" do
+  content node["public_key"]
 end
 
-# Install packages and software
+# recipes = [
+# 	"apt",
+# 	"build-essential",
+# 	"git",
+# 	"php",
+# 	"apache2",
+# 	"apache2::mod_php5",
+# 	"apache2::mod_rewrite",
+# 	"mysql::server",
+# 	"composer",
+# 	"nodejs::install_from_source",
+# ]
 
-packages = [
-	"nano",
-	"php5-curl",
-	"php5-mcrypt",
-	"php5-mysql",
-	"php5-gd",
-	"php5-imagick",
-	"php-apc",
-  "sendmail-bin",
-	"sendmail",
-]
+# for r in recipes do
+#   include_recipe r
+# end
 
-for p in packages do
-  package p do
-    action [:install]
-  end
-end
+# # Install packages and software
 
-execute "gem install compass" do
-  command "gem install compass"
-  action :run
-end
+# packages = [
+# 	"nano",
+# 	"php5-curl",
+# 	"php5-mcrypt",
+# 	"php5-mysql",
+# 	"php5-gd",
+# 	"php5-imagick",
+# 	"php-apc",
+#   "sendmail-bin",
+# 	"sendmail",
+# ]
 
-execute "npm install grunt" do
-  command "npm install -g grunt-cli"
-  action :run
-end
+# for p in packages do
+#   package p do
+#     action [:install]
+#   end
+# end
 
-execute "npm install forever" do
-  command "npm install -g forever"
-  action :run
-end
+# execute "gem install compass" do
+#   command "gem install compass"
+#   action :run
+# end
 
-# Add templates, fix permissions and add key
+# execute "npm install grunt" do
+#   command "npm install -g grunt-cli"
+#   action :run
+# end
 
-template "/usr/local/bin/devadd" do
-  source "devadd.erb"
-  owner "root"
-  group "staff"
-  mode "0755"
-end
+# execute "npm install forever" do
+#   command "npm install -g forever"
+#   action :run
+# end
 
-template "/usr/local/bin/chweb" do
-  source "chweb.erb"
-  owner "root"
-  group "staff"
-  mode "0755"
-end
+# # Add templates, fix permissions and add key
 
-execute "web permissions" do
-  command "chown -R root:www-data /var/www && chmod 2775 /var/www"
-  action :run
-end
+# template "/usr/local/bin/devadd" do
+#   source "devadd.erb"
+#   owner "root"
+#   group "staff"
+#   mode "0755"
+# end
 
-directory "/etc/php5/apache2" do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
-end
+# template "/usr/local/bin/chweb" do
+#   source "chweb.erb"
+#   owner "root"
+#   group "staff"
+#   mode "0755"
+# end
 
-template "/etc/php5/apache2/php.ini" do
-  source "php.ini-dev.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-end
+# execute "web permissions" do
+#   command "chown -R root:www-data /var/www && chmod 2775 /var/www"
+#   action :run
+# end
 
-if !node['cmd_lamp']['public_key'].nil? && !node['cmd_lamp']['public_key'].empty?
-  execute 'devadd' do
-    command "devadd #{node['cmd_lamp']['username']} \"#{node['cmd_lamp']['public_key']}\""
-  end
-end
+# directory "/etc/php5/apache2" do
+#   owner "root"
+#   group "root"
+#   mode "0755"
+#   action :create
+# end
 
-# Create virtual hosts
+# template "/etc/php5/apache2/php.ini" do
+#   source "php.ini-dev.erb"
+#   owner "root"
+#   group "root"
+#   mode "0644"
+# end
 
-sites = data_bag("sites")
+# # if !node['cmd_lamp']['public_key'].nil? && !node['cmd_lamp']['public_key'].empty?
+# #   execute 'devadd' do
+# #     command "devadd #{node['cmd_lamp']['username']} \"#{node['cmd_lamp']['public_key']}\""
+# #   end
+# # end
 
-sites.each do |s|
-	site = data_bag_item("sites", s)
-	web_app site["id"] do
-		template "site.conf.erb"
-	  server_name site["id"]
-	  docroot "/var/www/" + site["dir"]
-	end
-end
+# # Create virtual hosts
+
+# sites = data_bag("sites")
+
+# sites.each do |s|
+# 	site = data_bag_item("sites", s)
+# 	web_app site["id"] do
+# 		template "site.conf.erb"
+# 	  server_name site["id"]
+# 	  docroot "/var/www/" + site["dir"]
+# 	end
+# end
+
+
